b05e17f9b6524040a10adf0bde1104364d8d458c
jdxj <985759262@qq.com>
2020-06-11 23:45:41 +0800

    refactor: 避免并发初始化

--- email/email.go
+++ email/email.go
@@ -3,6 +3,7 @@ package email
 import (
 	"fmt"
 	"net/smtp"
+	"sync"
 
 	"github.com/jdxj/notice/config"
 
@@ -15,32 +16,46 @@ const (
 )
 
 var (
+	// mutex 保护 emailCfg 的初始化
+	mutex    = sync.Mutex{}
 	emailCfg *config.Email
 )
 
-func init() {
-	cfg, err := config.GetEmail()
-	if err != nil {
-		// todo
-		//panic(err)
+// 应用单例模式
+func getEmailCfg() (*config.Email, error) {
+	if emailCfg != nil {
+		return emailCfg, nil
 	}
-	emailCfg = cfg
+
+	var err error
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if emailCfg != nil {
+		return emailCfg, nil
+	}
+
+	emailCfg, err = config.GetEmail()
+	return emailCfg, err
 }
 
 func Send(subject string, data []byte, to ...string) error {
-	e := email.NewEmail()
+	cfg, err := getEmailCfg()
+	if err != nil {
+		return err
+	}
 
+	e := email.NewEmail()
 	e.Subject = subject
 	e.Text = data
-
-	e.From = fmt.Sprintf("notice <%s>", emailCfg.Addr)
+	e.From = fmt.Sprintf("notice <%s>", cfg.Addr)
 	e.To = to
 
-	return e.Send(addr, smtp.PlainAuth("", emailCfg.Addr, emailCfg.Token, host))
+	return e.Send(addr, smtp.PlainAuth("", cfg.Addr, cfg.Token, host))
 }
 
 func SendSelf(subject, content string) error {
-	self := emailCfg.Addr
+	self := "985759262@qq.com"
 
 	return Send(subject, []byte(content), self)
 }
