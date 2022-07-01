6698d73124542262ccb2a55325d56ef88f220194
wangyq <985759262@qq.com>
2022-07-01 17:52:42 +0800

    test

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,26 +5,12 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/jdxj/notice/logger"
-	"github.com/jdxj/notice/model/github"
-	"github.com/jdxj/notice/model/rss"
+	_ "github.com/jdxj/notice/subscription/github"
+	_ "github.com/jdxj/notice/subscription/rss"
 )
 
 func main() {
-	r := rss.NewRSS()
-	r.Start()
-
-	g := github.NewGithub()
-	g.Start()
-
-	logger.Infof("started")
-
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	<-c
-
-	r.Stop()
-	g.Stop()
-
-	logger.Infof("stopped")
 }
--- subscription/github/github_test.go
+++ subscription/github/github_test.go
@@ -5,7 +5,4 @@ import (
 )
 
 func TestGithub_T(t *testing.T) {
-	g := NewGithub()
-	g.getRepos()
-	g.run()
 }
