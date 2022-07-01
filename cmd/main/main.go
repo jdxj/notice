18ce63ba1aa0e5fcc260fe0c610a899d375f2e15
wangyq <985759262@qq.com>
2022-07-01 18:18:03 +0800

    test: api

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,12 +5,17 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/jdxj/notice/api/router"
 	_ "github.com/jdxj/notice/subscription/github"
 	_ "github.com/jdxj/notice/subscription/rss"
 )
 
 func main() {
+	router.Start()
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	<-c
+
+	router.Stop()
 }
